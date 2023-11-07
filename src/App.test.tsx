138bc1c8e24dba026b004c47fdad5672baab0edd
lkeitel <lkeitel@protonmail.com>
2023-11-07 17:07:11 -0600

    Add MUI and update app
    Adds MUI package and removes the boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('photo album showcase', () =>{
+  it('displays a textbox for entering the album number', () => {
+    render(<App />);
+    const albumTextBox = screen.getByLabelText('Album Number')
+    expect(albumTextBox).toBeInTheDocument();
+  });
+})
