53e57d06adcd04eb99a139272f553986ccfb1ac0
lkeitel <lkeitel@protonmail.com>
2023-11-08 15:25:17 -0600

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import React, {useState} from 'react';
 import AlbumTable from "./components/albumTable";
 import {AlbumDetail} from "./types";
 import AlbumForm from "./components/albumForm";
+import {Grid} from "@mui/material";
 
 function App() {
     const [albumDetails, setAlbumDetails] = useState<AlbumDetail[]>([])
 
     return (
         <div>
-            <h1 style={{textAlign: 'center'}}>Photo Album Showcase</h1>
-            <AlbumForm onSubmitAlbumForm={setAlbumDetails}/>
-            <AlbumTable albumDetails={albumDetails}/>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <h1 style={{textAlign: 'center'}}>Photo Album Showcase</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <AlbumForm onSubmitAlbumForm={setAlbumDetails}/>
+                </Grid>
+                <Grid item xs={12} style={{margin: 40}}>
+                    <AlbumTable albumDetails={albumDetails}/>
+                </Grid>
+            </Grid>
         </div>
   );
 }
--- src/components/albumTable.tsx
+++ src/components/albumTable.tsx
@@ -1,31 +1,86 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {
+    Paper,
+    styled,
+    Table,
+    TableBody,
+    TableCell,
+    tableCellClasses,
+    TableContainer,
+    TableHead, TablePagination,
+    TableRow
+} from "@mui/material";
 import {AlbumDetail} from "../types";
+import React, {useState} from "react";
+
 type Props = {
     albumDetails: AlbumDetail[]
 }
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
 export default function AlbumTable({albumDetails}: Props) {
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [page, setPage] = useState(0);
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
     return (
-        <TableContainer component={Paper} style={{margin: 30}}>
-            <Table>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+            <TableContainer  >
+                <Table >
                 <TableHead>
                     <TableRow>
-                        <TableCell>Album Id</TableCell>
-                        <TableCell>Song Id</TableCell>
-                        <TableCell>Song Title</TableCell>
-                        <TableCell>Photo</TableCell>
+                        <StyledTableCell>Album Id</StyledTableCell>
+                        <StyledTableCell>Song Id</StyledTableCell>
+                        <StyledTableCell>Song Title</StyledTableCell>
+                        <StyledTableCell>Photo</StyledTableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {albumDetails.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.albumId}</TableCell>
-                            <TableCell>{row.id}</TableCell>
-                            <TableCell>{row.title}</TableCell>
-                            <TableCell><img src={row.thumbnailUrl}/></TableCell>
-                        </TableRow>
+                    {albumDetails
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => (
+                        <StyledTableRow key={row.id}>
+                            <StyledTableCell>{row.albumId}</StyledTableCell>
+                            <StyledTableCell>{row.id}</StyledTableCell>
+                            <StyledTableCell>{row.title}</StyledTableCell>
+                            <StyledTableCell><img src={row.thumbnailUrl} width={50} height={50}/></StyledTableCell>
+                        </StyledTableRow>
                     ))}
                 </TableBody>
             </Table>
         </TableContainer>
+        <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={albumDetails.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        </Paper>
     )
 }
